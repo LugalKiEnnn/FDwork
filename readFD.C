913bbd78829c9ff6d6c7f808a70a5a837149ba5f
LugalKiEnnn <82413350+LugalKiEnnn@users.noreply.github.com>
2021-05-08 10:59:52 +0300

    Update readFD.C

--- readFD.C
+++ readFD.C
@@ -3,9 +3,6 @@
 #include <TClonesArray.h>
 #include <TH1.h>
 
-// @(#)bmnroot/macro/howTo:$Id$
-// Author: Pavel Batyuk <pavel.batyuk@jinr.ru> 2017-07-18
-
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
 // readEve.C                                                                  //
@@ -32,32 +29,71 @@ void readFD(TString fileName = "bmnsim.root") {
 
     //Creating histograms
     TH1I *histAmp = new TH1I("histAmp", "Amplitude distribution;Amplitude;Number", 100, 0, 0.01);
-    TH1I *histMod = new TH1I("histMod", "Module distribution;Module;Number", 65, -1, 63);
+    TH1I *histAmpOld = new TH1I("histAmpOld", "Amplitude distribution;Amplitude;Number", 100, 0, 0.01);
+    TH1I *histAmpDif = new TH1I("histAmpDif", "Amplitude distribution;Amplitude;Number", 100, 0, 0.01);
+    TH1I *histMod = new TH1I("histMod", "Module distribution;Module;Number", 67, -2, 65);
+    TH1I *histModOld = new TH1I("histModOld", "Module distribution;Module;Number", 67, -2, 65);
+    TH1I *histModDif = new TH1I("histModDif", "Module distribution;Module;Number", 67, -2, 65);
+    auto hStackMod = new THStack("hStackMod","Old and filtered digit modules");
+    auto hStackAmp = new THStack("hStackAmp","Old and filtered digit amplitudes");
     TFile f("histo.root", "recreate");
 
     TClonesArray* fdDigits = nullptr;
     out->SetBranchAddress("FDDigit", &fdDigits);
 
     Int_t nEntries = 0;
+    Int_t nEntriesOld = 0;
 
+    histMod->SetLineColor(1);
+    histAmp->SetLineColor(1);
+    histModOld->SetLineColor(2);
+    histAmpOld->SetLineColor(2);
+    histModDif->SetLineColor(2);
+    histAmpDif->SetLineColor(2);
 
     for (Int_t iEv = 0; iEv < out->GetEntries(); iEv++) {
         out->GetEntry(iEv);
         
-        if( fdDigits->GetEntriesFast() > 2 ) {
-            for (Int_t iDigit = 0; iDigit < fdDigits->GetEntriesFast(); iDigit++) {
+        for (Int_t iDigit = 0; iDigit < fdDigits->GetEntriesFast(); iDigit++) {
+            BmnTrigDigit* digi = (BmnTrigDigit*) fdDigits->UncheckedAt(iDigit);
+                        
+            nEntriesOld++;
+            histModOld->Fill(digi->GetMod());
+            histAmpOld->Fill(digi->GetAmp());
+
+            if( fdDigits->GetEntriesFast() > 2 ) {
                 nEntries++;
-                BmnTrigDigit* digi = (BmnTrigDigit*) fdDigits->UncheckedAt(iDigit);
-                
-                histMod->AddBinContent(digi->GetMod());
+                histMod->Fill(digi->GetMod());
                 histAmp->Fill(digi->GetAmp());
-            
             }
+        
         }
-    }   
+    }
+
+    histModDif->Add(histMod,histModOld, -1, 1);
+    histAmpDif->Add(histAmp,histAmpOld, -1, 1);
+
+    hStackAmp->Add(histAmp);
+    hStackMod->Add(histMod);
+    hStackMod->Add(histModDif);
+    hStackAmp->Add(histAmpDif);
+
+    hStackMod->Draw();
+    hStackMod->GetXaxis()->SetTitle("Module");
+    hStackMod->GetYaxis()->SetTitle("Number");
+
+    hStackAmp->Draw();
+    hStackAmp->GetXaxis()->SetTitle("Amplitude");
+    hStackAmp->GetYaxis()->SetTitle("Number");
     
     histMod->SetEntries(nEntries); 
     histAmp->SetEntries(nEntries);
     histMod->Write();
     histAmp->Write();
+
+    histModOld->Write();
+    histModDif->Write();
+
+    hStackMod->Write();
+    hStackAmp->Write();
 }
